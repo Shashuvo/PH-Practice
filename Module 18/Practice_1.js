2490d45720f00dc9291a6871851d9abbce9a5dc5
Shashuvo <shahariat2810@gmail.com>
2025-02-15 22:14:53 +0600

    If else statement practice

---
+++ Module 18/Practice_1.js
@@ -0,0 +1,17 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+// Price of Burger
+const burgerPrice = 325;
+// if else statement 
+if(burgerPrice > 500 ){
+    console.log("Coke is free");
+}
+else{
+    console.log("30 Tk");
+}
