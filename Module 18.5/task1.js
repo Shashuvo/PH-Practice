a1c1b776a03a51e380c630ac9f61929d06a5cede
Shashuvo <shahariat2810@gmail.com>
2025-02-17 21:08:42 +0600

    Array declare, add and delete element Practice

---
+++ Module 18.5/task1.js
@@ -0,0 +1,17 @@
+//Declare an array
+
+// 1. Declare an array with 5 elements containing fruits
+const fruits=['Mango', 'Guava', 'Orange', 'Lemon', 'Berry'];
+
+console.log(fruits);
+
+// 2. console log the 3rd index element
+
+console.log(fruits[2]);
+
+// 3. change the value of the 2nd index element to jambura
+
+fruits[1]='Jambura';
+
+// 4. console log the final array
+console.log(fruits);
---
+++ Module 18.5/task2.js
@@ -0,0 +1,24 @@
+// 2. Add or remove elements
+
+// 1. Declare an array of 3 tourist destinations
+
+const destinations=['Patenga', 'Akilpur', 'Navel'];
+console.log(destinations);
+
+// 2. Add a new tourist destination to your tourist array
+
+destinations.push('Bhatiary Lake');
+console.log(destinations);
+
+// 3. Add two more to your array
+destinations.push('Guliakhali');
+destinations.push('Kaptai');
+console.log(destinations);
+
+// 4. Remove the last tourist destination you have added
+
+destinations.pop();
+
+
+// 5. display the final array as output
+console.log(destinations);
