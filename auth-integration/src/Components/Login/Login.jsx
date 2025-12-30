19a9ff2a076fd1e514cddbe89ca806dd0c5cb63c
Shashuvo <shahariat2810@gmail.com>
2025-12-30 23:31:33 +0600

    Login & register form added & fb setup

--- auth-integration/src/Components/Login/Login.jsx
+++ auth-integration/src/Components/Login/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login.</h1>
+        <div className="card bg-base-100 border-2 border-black w-full mx-auto mt-10 max-w-sm shrink-0 shadow-2xl">
+            <h1 className="text-4xl text-center pl-5 pt-5 font-bold">Login now!</h1>
+            <div className="card-body">
+                <form className="fieldset">
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                    <div>
+                        <a className="link link-hover">Forgot password?</a>
+                    </div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>Don't have an account?<Link to="/register" className="text-blue-600 hover:underline font-semibold"> Register</Link></p>
+            </div>
         </div>
     );
 };
--- auth-integration/src/Components/Register/Register.jsx
+++ auth-integration/src/Components/Register/Register.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-            <h1>This is Register.</h1>
+        <div className="card bg-base-100 border-2 border-black w-full mx-auto mt-10 max-w-sm shrink-0 shadow-2xl">
+            <h1 className="text-4xl text-center pl-5 pt-5 font-bold">Login now!</h1>
+            <div className="card-body">
+                <form className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="text" className="input" placeholder="Your Full Name" />
+                    <label className="label">Email</label>
+                    <input type="email" className="input" placeholder="Email" />
+                    <label className="label">Password</label>
+                    <input type="password" className="input" placeholder="Password" />
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>Already have an account?<Link to="/login" className="text-blue-600 hover:underline font-semibold"> Login</Link></p>
+            </div>
         </div>
     );
 };
