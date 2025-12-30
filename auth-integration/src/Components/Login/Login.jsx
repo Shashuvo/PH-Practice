1704b204564aef4ac71de2072d746558cdac9abe
Shashuvo <shahariat2810@gmail.com>
2025-12-31 00:38:29 +0600

    login handled via Auth Provider

--- auth-integration/src/Components/Login/Login.jsx
+++ auth-integration/src/Components/Login/Login.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Login = () => {
+    const { signInUser } = use(AuthContext);
+    const handleSignIn = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        // sign in
+        signInUser(email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
         <div className="card bg-base-100 border-2 border-black w-full mx-auto mt-10 max-w-sm shrink-0 shadow-2xl">
             <h1 className="text-4xl text-center pl-5 pt-5 font-bold">Login now!</h1>
             <div className="card-body">
-                <form className="fieldset">
+                <form onSubmit={handleSignIn} className="fieldset">
                     <label className="label">Email</label>
-                    <input type="email" className="input" placeholder="Email" />
+                    <input type="email" name="email" className="input" placeholder="Email" />
                     <label className="label">Password</label>
-                    <input type="password" className="input" placeholder="Password" />
+                    <input type="password" name="password" className="input" placeholder="Password" />
                     <div>
                         <a className="link link-hover">Forgot password?</a>
                     </div>
